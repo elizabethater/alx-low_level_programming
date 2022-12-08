28c75bcaa4cd593f312dbadd88bdf60b679d3922
elizabethater <aterelizabeth26@gmail.com>
2022-12-08 13:28:04 -0800

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-
 /**
- * main - Entry Point
- *
- * Return: Always 0 (Success)
+ * main - Entry
+ * Return: Always 0
  */
-
-int main(void) 
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
