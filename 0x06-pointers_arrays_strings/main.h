95d1fcd20107ed0a5ed537d750b46a4d016f3d29
elizabethater <aterelizabeth26@gmail.com>
2022-12-22 08:53:48 -0800

    task6

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,7 +8,7 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *a);
 char *cap_string(char *str);
-char *leet(char *n);
+char *leet(char *a);
 int _putchar(char c);
 
 #endif
